990fd86916a3c91ab8d6e12b9af85855c8a3dac5
TraderXylioner <treider.xylioner@gmail.com>
2024-09-06 17:08:58 +0300

    add EventViewSet, EventRegistrationViewSet

--- event_management/events/views.py
+++ event_management/events/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from .models import Event, Registration
+from .serializers import EventSerializer, EventRegistrationSerializer
 
-# Create your views here.
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class EventRegistrationViewSet(viewsets.ModelViewSet):
+    queryset = Registration.objects.all()
+    serializer_class = EventRegistrationSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
