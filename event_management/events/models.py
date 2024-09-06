3e088602bb193eed63e3c7479fbc04cc82e6b3a4
TraderXylioner <treider.xylioner@gmail.com>
2024-09-06 17:08:31 +0300

    add Registration model

--- event_management/events/models.py
+++ event_management/events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Event(models.Model):
@@ -8,3 +9,8 @@ class Event(models.Model):
     date = models.DateTimeField()
     location = models.CharField(max_length=255)
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Registration(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
